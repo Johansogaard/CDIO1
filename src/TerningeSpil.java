2b65e3552b00a28aa8a12dfb03a1148e72169642
victor <Gingerlord.and.co@gmail.com>
2022-09-21 09:30:29 +0200

    dobbelt 6'er * 2 vinder hvis det virker
    mere test nødvendig.
    
    skal gøres smartere

--- src/TerningeSpil.java
+++ src/TerningeSpil.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+
 import java.util.Random;
 class TerningeSpil {
     public static void main( String args[] ) {
@@ -6,15 +6,16 @@ class TerningeSpil {
         //variblerne bliver difineret tror jeg
         int terningmax = 6;
         int terningmin = 1;
-        int player1=0;
-        int player2=0;
-        int terning11=0, terning12=0, terning21=0, terning22=0;
+        int player1=0; //player 1 score
+        int player2=0; //player 2 score
+        int terning11=0, terning12=0, terning21=0, terning22=0; //terning
+        int pterning1=0, pterning2=0;
 
         System.out.println("Hej og velkommen til spil");
         System.out.println("klik enter for at starte, og fortsætte spillet");
 
         //spil løkken
-        while (true){
+        outerloop: while (true){
 
             //enter for rul
                 try{System.in.read();}
@@ -23,13 +24,20 @@ class TerningeSpil {
                 //tilfældigt tal fra 0-5 + 1 = terning
             terning11=rand.nextInt(terningmax)+terningmin;
             terning12=rand.nextInt(terningmax)+terningmin;
-
-            //spiller score bliver  opdateret og printet
             player1=player1+terning11+terning12;
-
             System.out.println("spiller 1 slår "+(terning11)+" og "+(terning12)+" spiller 1 score er "+(player1));
+
+
+            //terning11+terning12
+            //spiller score bliver  opdateret og printet
+
+//6'ere to gange
+            if (terning11+terning12==pterning1 && pterning1==12)
+                break;
+            pterning1=terning11+terning12;
             // der bliver testet om der er blevet slået to ens efter 40
-            if ((player1>40 && terning11==terning12) )
+
+            if ((player1>40 && terning11==terning12))
                 break;
 
 
@@ -47,11 +55,21 @@ class TerningeSpil {
                 //tilfældigt tal fra 0-5 + 1 = terning
                 terning11=rand.nextInt(terningmax)+terningmin;
                 terning12=rand.nextInt(terningmax)+terningmin;
+
+
                 //spiller score bliver  opdateret og printet
                 player1=player1+terning11+terning12;
                 System.out.println("spiller 1 slår "+(terning11)+" og "+(terning12)+" spiller 1 score er "+(player1));
+                // kan ikke lave double break
+                if (terning11+terning12==pterning1 && pterning1==12)
+                    break outerloop;
+                pterning1=terning11+terning12;
+
+                if (terning11==1 && terning12==1) {
+                    System.out.println("to 1'ere, tilbage til start");
+                    player1 = 0;
             }
-            if ((player1>40 && terning11==terning12) ) //|| (player2>40 && terning21==terning22)
+            if ((player1>40 && terning11==terning12) ) //
                 break;
 
 
@@ -67,7 +85,11 @@ class TerningeSpil {
 
             System.out.println("spiller 2 slår "+(terning21)+" og "+(terning22)+" spiller 2 score er "+(player2));
 
-            if ((player2>40 && terning11==terning12) ) //|| (player2>40 && terning21==terning22)
+                if (terning21+terning22==pterning2 && pterning2==12)
+                    break;
+                pterning2=terning21+terning22;
+
+            if ((player2>40 && terning11==terning12) ) //
                 break;
 
             if (terning21+terning22==2) {
@@ -87,14 +109,20 @@ class TerningeSpil {
 
                 System.out.println("spiller 2 slår "+(terning21)+" og "+(terning22)+" spiller 2 score er "+(player2));
 
+                if (terning11+terning12==pterning1 && pterning1==12)
+                    break outerloop;
+                pterning1=terning11+terning12;
 
+                if (terning21+terning22==2) {
+                    System.out.println("to 1'ere, tilbage til start");
+                    player2 = 0;
             }
 
             if ((player2>40 && terning11==terning12) ) //|| (player2>40 && terning21==terning22)
                 break;
-        }
+        }}}
 //vi kommer ud af spil løkken når en kommer over 40
-        if (terning21==terning22)
+        if (terning21==terning22 || pterning2==terning21+terning22)
             System.out.println("player 2 vinder med "+(player2));
         else
             System.out.println("player 1 vinder med "+(player1));
