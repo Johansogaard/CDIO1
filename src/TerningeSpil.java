604b0fc6443328672762d17b4a88acbce5d88a6f
Anders Jefsen <andersjeff397@gmail.com>
2022-09-21 11:17:29 +0200

    ja nemlig, ja

--- src/TerningeSpil.java
+++ src/TerningeSpil.java
@@ -3,7 +3,7 @@ import java.util.Random;
 class TerningeSpil {
     public static void main( String args[] ) {
         Random rand = new Random(); //instance of random class
-        //variablerne bliver defineret tror jeg
+        //variablerne bliver defineret tror jeg, thats right
         int terningmax = 6;
         int terningmin = 1;
         int player1=0; //player 1 score
